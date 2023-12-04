3d5d945c03b75a4a3e00f8fd5648795d88417417
Jan <jan0984327881@gmail.com>
2023-12-04 15:06:22 +0800

    12/4

--- homework/hw1.py
+++ homework/hw1.py
@@ -1,1 +1,17 @@
-print('hello')
+def fibonacci_iterative(n):
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer.")
+
+    fib = [0, 1]
+
+    for i in range(2, n + 1):
+        fib.append(fib[i - 1] + fib[i - 2])
+
+    return fib[n]
+
+# Example usage:
+n = 1
+# n = 40
+# n = 60
+result = fibonacci_iterative(n)
+print(f'fibonacci({n})={result}')
